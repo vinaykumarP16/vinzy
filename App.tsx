7860d02e681d2e5286682d6513660dc6dfcf701c
vinaykumarP16 <vinaykumar.p1995v@gmail.com>
2023-03-10 12:06:21 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+
+  const myCollections =[
+    {
+      name:'Chat gpt',
+      site:"",
+    },
+    {
+      name:'LinkedIn',
+      site:"",
+    },
+    {
+      name:'YouTube',
+      site:"",
+    },
+    {
+      name:'WhatsApp',
+      site:"",
+    },
+    {
+      name:''
+    }
+
+  ]
+
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
